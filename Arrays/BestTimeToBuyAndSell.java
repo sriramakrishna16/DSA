27b29d6e4b23f60efc06c7bdb91ddb025d22601e
sriramakrishna16 <srirambvc16@gmail.com>
2026-03-30 20:25:05 +0530

    BestTimeToBuyAndSell solved

---
+++ Arrays/BestTimeToBuyAndSell.java
@@ -0,0 +1,31 @@
+public class BestTimeToBuyAndSell {
+    public static void main(String [] args){
+        int [] stockPrices = {7,1,5,3,6,19,4};
+        int profit = findBestProfit(stockPrices);
+        System.out.println(profit);
+    }
+
+    public static int findBestProfit(int[] prices){
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        
+        for(int price : prices){
+
+            if(price<minPrice){
+                minPrice = price;
+            }
+
+            int profit = price-minPrice;
+
+            // maxProfit = Math.max(maxProfit,profit);
+            if(profit>maxProfit){
+                maxProfit = profit;
+            }
+
+        }
+
+        return maxProfit;
+    }
+}
+
+
