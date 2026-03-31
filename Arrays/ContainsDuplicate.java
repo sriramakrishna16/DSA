8b67e8c17c58605b68787449e63c1020a12006b2
sriramakrishna16 <srirambvc16@gmail.com>
2026-03-31 22:58:07 +0530

    Reapply "DSA Problems solved"
    This reverts commit 8caf67457eca1a30f9eacc4819476833087f2930.

--- Arrays/ContainsDuplicate.java
+++ Arrays/ContainsDuplicate.java
@@ -1,7 +1,5 @@
 package Arrays;
-
 import java.util.HashSet;
-
 
 public class ContainsDuplicate {
     public static void main(String args[]){
--- Arrays/MaximumSubArray.java
+++ Arrays/MaximumSubArray.java
@@ -1,3 +1,5 @@
+package Arrays;
+
 public class MaximumSubArray {
     public static void main(String [] args){
         int nums[] = {-2,1,-3,4,-1,2,1,-5,4};
