eb53ce23c75f6b528053a1a0f1ccd3e729b88983
sriramakrishna16 <srirambvc16@gmail.com>
2026-05-24 20:03:33 +0530

    Jump game problem solved

---
+++ DynamicProgramming/JumpGame.java
@@ -0,0 +1,33 @@
+package DynamicProgramming;
+
+import java.util.Arrays;
+
+//problem states that at each index given a value , so that we use that value to max jump from index -
+// or current position. so that if we can reach last index return true or else false.
+// for this ,dp is not a optimal solution because we only look forward and unwanted calculations are done.
+//example ,value = 3 , we can use 3 to reach end , but dp calculates between values to reach
+
+public class JumpGame {
+    public static void main(String args[]){
+        int nums[] = {2,3,1,1,4};
+        boolean ans = jump(nums);
+        System.out.println(ans);
+    }
+    public static boolean jump(int[] nums){
+        int len = nums.length;
+        boolean [] dp = new boolean [len];
+        Arrays.fill(dp,false);
+        dp[0] = true;
+
+        for(int i = 0; i < len-1 ;i++){
+            if(dp[i] == true){
+                int maxJump = Math.min(len-1, i + nums[i]); //2
+                for(int j = i + 1; j <= maxJump;j++){ //
+                        dp[j] = true;
+                }
+            }
+        }
+        return dp[len - 1];
+    }
+
+}
