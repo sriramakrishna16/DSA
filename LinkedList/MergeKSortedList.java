c65f0509d9ff098bf20c6cea89a13b2db1323dfa
sriramakrishna16 <srirambvc16@gmail.com>
2026-06-11 20:21:50 +0530

    Optimised space complexity solution added

--- LinkedList/MergeKSortedList.java
+++ LinkedList/MergeKSortedList.java
@@ -37,34 +37,90 @@ public class MergeKSortedList {
 
         MyLinkedList.printValues(ans);
     }
-    public static ListNode mergeAll(ListNode[] list){
+    // public static ListNode mergeAll(ListNode[] list){
 
-        //here data type is ListNode so we have provide the comparator. bcz java doesnt know about ListNode
-        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a,b)->a.val-b.val);
+    //     //here data type is ListNode so we have provide the comparator. bcz java doesnt know about ListNode
+    //     PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a,b)->a.val-b.val);
 
-        //adding all first nodes in minHeap
-        for(ListNode node : list){
-            if(node != null){
-                minHeap.offer(node);
-            }
+    //     //adding all first nodes in minHeap
+    //     for(ListNode node : list){
+    //         if(node != null){
+    //             minHeap.offer(node);    //K
+    //         }
+    //     }
+
+    //     ListNode dummy = new ListNode(-1);
+    //     ListNode current = dummy;
+
+    //     //attaching smallest node to the dummy
+    //     while(!minHeap.isEmpty()){
+
+    //         ListNode smallest = minHeap.poll();
+
+    //         current.next = smallest;
+
+    //         current = current.next;
+
+    //         //if list has remaining nodes add it to the minHeap
+    //         if(smallest.next != null){
+    //             minHeap.offer(smallest.next);
+    //         }
+    //     }
+
+    //     return dummy.next;
+    // }
+
+
+    //more optimised version space complexity O(1)
+    //using iterative divide and conquer method
+    public static ListNode mergeAll(ListNode[] lists) {
+
+        if (lists == null || lists.length == 0) {
+            return null;
         }
 
-        ListNode dummy = new ListNode(-1);
-        ListNode current = dummy;
+        int interval = 1;
 
-        //attaching smallest node to the dummy
-        while(!minHeap.isEmpty()){
+        while (interval < lists.length) {
 
-            ListNode smallest = minHeap.poll();
+            for (int i = 0; i + interval < lists.length; i += interval * 2) {
 
-            current.next = smallest;
-
-            current = current.next;
-
-            //if list has remaining nodes add it to the minHeap
-            if(smallest.next != null){
-                minHeap.offer(smallest.next);
+                lists[i] = mergeTwoLists(
+                        lists[i],
+                        lists[i + interval]
+                );
             }
+
+            interval *= 2;
+        }
+
+        return lists[0];
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+
+            if (l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+
+            tail = tail.next;
+        }
+
+        if (l1 != null) {
+            tail.next = l1;
+        }
+
+        if (l2 != null) {
+            tail.next = l2;
         }
 
         return dummy.next;
