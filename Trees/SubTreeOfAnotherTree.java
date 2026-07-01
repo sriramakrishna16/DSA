7c68feba37648d9a28ae5125620ba6dbacfa3798
sriramakrishna16 <srirambvc16@gmail.com>
2026-07-01 20:44:54 +0530

    serialization method also added

--- Trees/SubTreeOfAnotherTree.java
+++ Trees/SubTreeOfAnotherTree.java
@@ -1,5 +1,8 @@
 package Trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 //problem , given a tree and subtree
 //so that we have to find that subtree presents in tree or not
 //if found , return true . if not return false.
@@ -7,7 +10,7 @@ package Trees;
 //i used recursion to solve this problem
 
 public class SubTreeOfAnotherTree {
-    public static void main(String args[]){
+    /* public static void main(String args[]){
         Integer [] arr = {3,4,5,1,2};
         Integer [] arr2 = {4,1,2};
 
@@ -16,8 +19,10 @@ public class SubTreeOfAnotherTree {
 
         TreeNode.printLevelOrder(root);
         System.out.println();
+
         TreeNode.printLevelOrder(root1);
         System.out.println();
+
         boolean ans = isSubTree(root, root1);
         System.out.println("sub tree contains ? :" + ans);
     }
@@ -47,5 +52,47 @@ public class SubTreeOfAnotherTree {
         }
 
         return isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right);
+    } */
+
+
+    //another optimised solution 
+    public static void main(String args[]){
+        Integer [] arr = {12};
+        Integer [] arr2 = {2};
+
+        TreeNode root = TreeNode.buildTreeLevelOrder(arr);
+        TreeNode root1 = TreeNode.buildTreeLevelOrder(arr2);
+
+        TreeNode.printLevelOrder(root);
+        System.out.println();
+
+        TreeNode.printLevelOrder(root1);
+        System.out.println();
+
+        String tree = serialize(root);
+        String subTree = serialize(root1);
+        System.out.println(tree);
+        System.out.println(subTree);
+
+        System.out.println(tree.contains(subTree));
+    }
+
+        
+    public static String serialize(TreeNode root){
+        StringBuilder sb = new StringBuilder();
+        preorder(root, sb);
+        return sb.toString();
+    }
+
+    public static void preorder(TreeNode root , StringBuilder sb){
+        if(root == null){
+            sb.append("#,");
+            return;
+        }
+
+        sb.append("$").append(root.val).append(",");
+
+        preorder(root.left, sb);
+        preorder(root.right, sb);
     }
 }
