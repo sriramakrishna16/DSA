4a70218f83e90869044f2b3fe6546538be8dd928
sriramakrishna16 <srirambvc16@gmail.com>
2026-07-16 20:18:56 +0530

    Word pattern problem solution added

---
+++ HashMap/WordPattern.java
@@ -0,0 +1,44 @@
+package HashMap;
+
+import java.util.HashMap;
+
+public class WordPattern {
+    public static void main(String args[]){
+        String pattern = "abba";
+        String s = "dog cat cat dog";
+        boolean ans = wordPattern(pattern, s);
+        System.out.println(ans);
+    }
+    public static boolean wordPattern(String pattern , String s){
+        String words [] = s.split(" ");
+        if(pattern.length() != words.length){
+            return false;
+        }
+        HashMap<Character, String> map1 = new HashMap<>();
+        HashMap<String, Character> map2 = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            String word = words[i];
+
+            if (map1.containsKey(ch)) {
+                if (!map1.get(ch).equals(word)) {
+                    return false;
+                }
+            } else {
+                map1.put(ch, word);
+            }
+
+            if (map2.containsKey(word)) {
+                if (map2.get(word) != ch) {
+                    return false;
+                }
+            } else {
+                map2.put(word, ch);
+            }
+        }
+
+        return true;
+
+    }
+}
