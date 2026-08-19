e64846231294211b1cc53131ddab30717f3eb947
sriramakrishna16 <srirambvc16@gmail.com>
2026-08-19 23:13:50 +0530

    Smallest letetr greater than target solution added

---
+++ BinarySearch/SmallestLetterGreaterThanTarget.java
@@ -0,0 +1,22 @@
+public class SmallestLetterGreaterThanTarget{
+    public static void main(String [] args){
+        char [] letters = {'c','f','j'};
+        char target = 'a';
+        char ans = findLetter(letters,target);
+        System.out.println(ans);
+    }
+
+    public static char findLetter(char[] letters, char target){
+        int left = 0;
+        int right = letters.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return letters[left % letters.length];
+    }
+}
