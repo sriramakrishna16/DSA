911829eb4a133ca850d37ed27342d7e6164bfc7f
sriramakrishna16 <srirambvc16@gmail.com>
2026-08-25 23:21:07 +0530

    Single element in sorted array solution added

---
+++ BinarySearch/SingleElementInSortedArray.java
@@ -0,0 +1,23 @@
+public class SingleElementInSortedArray {
+    public static void main(String [] args){
+        int [] nums = {1,1,2,3,3,4,4,8,8};
+        int ans = singleNonDuplicate(nums);
+        System.out.println(ans);
+    }
+    public static int singleNonDuplicate(int [] nums){
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] == nums[mid + 1]) {
+                left = mid + 2;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
